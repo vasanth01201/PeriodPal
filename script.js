cec6f779c207d7af4ddd999ea4dc21d315249b1c
vasanth01201 <108180813+vasanth01201@users.noreply.github.com>
2022-06-25 18:26:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function prod1(){
+  document.getElementById('prod1').style.background = "#000";
+  document.getElementById('prod2').style.background = "#f37771";
+  document.getElementById('prod3').style.background = "#f37771";
+  document.getElementById('prod4').style.background = "#f37771";
+
+}
+
+function prod2(){
+  document.getElementById('prod1').style.background = "#f37771";
+  document.getElementById('prod2').style.background = "#000";
+  document.getElementById('prod3').style.background = "#f37771";
+  document.getElementById('prod4').style.background = "#f37771";
+}
+
+function prod3(){
+  document.getElementById('prod1').style.background = "#f37771";
+  document.getElementById('prod2').style.background = "#f37771";
+  document.getElementById('prod3').style.background = "#000";
+  document.getElementById('prod4').style.background = "#f37771";
+}
+
+function prod4(){
+  document.getElementById('prod1').style.background = "#f37771";
+  document.getElementById('prod2').style.background = "#f37771";
+  document.getElementById('prod3').style.background = "#f37771";
+  document.getElementById('prod4').style.background = "#000";
+}
